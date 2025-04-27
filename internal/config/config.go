810689c823efe9bba8dccc83c21a67e8ebb14a89
Cooper Standard <cooper@cooperstandard.org>
2025-04-27 00:16:42 -0700

    user table and refactor config internal

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,28 +6,56 @@ import (
 	"path/filepath"
 )
 
-func getConfigFilePath() (string, error) {
-	_, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-	fullPath := filepath.Join(".", "gatorConfig.json")
-	return fullPath, nil
-}
+const configFileName = ".gatorconfig.json"
 
 type Config struct {
-	DBURL           string `json:"db_url`
+	DBURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func (cfg *Config) SetUser(name string) error {
-	cfg.CurrentUserName = name
+func (cfg *Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
 	return write(*cfg)
 }
 
+func Read() (Config, error) {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(fullPath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func getConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	fullPath := filepath.Join(home, configFileName)
+	return fullPath, nil
+}
+
 func write(cfg Config) error {
-	path, _ := getConfigFilePath()
-	file, err := os.Create(path)
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(fullPath)
 	if err != nil {
 		return err
 	}
@@ -38,25 +66,6 @@ func write(cfg Config) error {
 	if err != nil {
 		return err
 	}
+
 	return nil
-}
-
-func Read() (Config, error) {
-	path, _ := getConfigFilePath()
-
-	file, err := os.Open(path)
-	if err != nil {
-		return Config{}, err
-	}
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-
-	cfg := Config{}
-	err = decoder.Decode(&cfg)
-	if err != nil {
-		return Config{}, err
-	}
-
-	return cfg, nil
 }
